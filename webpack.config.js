633a681d9e324f861e3b8db53ebc470b7e5f3cda
HelplessSoldier <patrick1234143211@gmail.com>
2023-08-10 14:04:39 -0700

    initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './template.html',
+        }),
+    ],
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
